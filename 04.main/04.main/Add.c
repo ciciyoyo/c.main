44ae6054d595de898ace9078314d73f927f1cdd0
Rido <2151241@qq.com>
2022-04-12 16:28:55 +0800

    c_main

---
+++ 04.main/04.main/Add.c
@@ -0,0 +1,8 @@
+//
+//  Add.c
+//  04.main
+//
+//  Created by Rido on 2022/4/12.
+//
+
+#include "Add.h"
---
+++ 04.main/04.main/Add.h
@@ -0,0 +1,13 @@
+//
+//  Add.h
+//  04.main
+//
+//  Created by Rido on 2022/4/12.
+//
+
+#ifndef Add_h
+#define Add_h
+
+#include <stdio.h>
+
+#endif /* Add_h */
