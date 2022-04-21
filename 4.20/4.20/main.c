7cc2171d51a9ab76394ea66c150fd0ff2e5f0054
Rido <2151241@qq.com>
2022-04-21 15:00:20 +0800

    i

--- 4.20/4.20/main.c
+++ 4.20/4.20/main.c
@@ -6,9 +6,10 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 //经典笔试题
 int main(int argc, const char * argv[]) {
-    //整型数组
+    //整型数组大小
     int a[] = {1, 2, 3, 4};
     printf("%lu\n", sizeof(a));//16
     printf("%lu\n", sizeof(a+0));//4/8首元素地址加0还是首元素地址
@@ -20,7 +21,8 @@ int main(int argc, const char * argv[]) {
     printf("%lu\n", sizeof(&a+1));//4/8越界的地址
     printf("%lu\n", sizeof(&a[0]));//4/8取首元素地址
     printf("%lu\n", sizeof(&a[0]+1));//4/8取第二个元素的地址
-    //字符数组
+    printf("*****************\n");
+    //字符数组大小
     char car[] = {'a', 'b', 'c', 'd', 'e', 'f'};
     printf("%lu\n", sizeof(car));//6
     printf("%lu\n", sizeof(car+0));//4/8
@@ -29,6 +31,75 @@ int main(int argc, const char * argv[]) {
     printf("%lu\n", sizeof(&car));//4/8
     printf("%lu\n", sizeof(&car+1));//4/8
     printf("%lu\n", sizeof(&car[0]+1));//4/8
+    printf("*****************\n");
+    //字符串长度
+    printf("%lu\n", strlen(car));//随机
+    printf("%lu\n", strlen(car + 0));//随机
+    //'a'/97 而strlen需要传指针，而你把97作为地址向后找\0时非法访问
+//    printf("%lu\n", strlen(*car));//报错
+//    printf("%lu\n", strlen(car[1]));//err
+    printf("%lu\n", strlen(&car));//随机
+    printf("%lu\n", strlen(&car+1));//也是随机和上个随机值差6
+    printf("%lu\n", strlen(&car[0]+1));//也是随机和上个随机差1
+    printf("*****************\n");
+    //字符数组
+    char sar[] = "asdzxc";
+    printf("%lu\n", sizeof(sar));//7
+    printf("%lu\n", sizeof(sar + 0));//4/8
+    printf("%lu\n", sizeof(*sar));//1
+    printf("%lu\n", sizeof(sar[1]));//1
+    printf("%lu\n", sizeof(&sar));//4/8
+    printf("%lu\n", sizeof(&sar+1));//4/8
+    printf("%lu\n", sizeof(&sar[0]+1));//4/8
+    printf("*****************\n");
+    printf("%lu\n", strlen(sar));//6
+    printf("%lu\n", strlen(sar + 0));//6
+//    printf("%lu\n", strlen(*sar));//err
+//    printf("%lu\n", strlen(sar[1]));//err
+    //警告char (*)[7]与const char*类型不一样
+    printf("%lu\n", strlen(&sar));//6
+    //警告char (*)[7]跳过这个数组还是一个数组
+    printf("%lu\n", strlen(&sar+1));//随机
+    printf("%lu\n", strlen(&sar[0]+1));//5
+    printf("*****************\n");
+    //字符指针pointer
+    char *p = "asdzxc";//常量字符串不可以改的，而字符数组时可以改的
+    printf("%lu\n", sizeof(p));//4/8
+    printf("%lu\n", sizeof(p + 1));//4/8
+    printf("%lu\n", sizeof(*p));//1
+    //arr[0] == *(arr+0) 等价的 p[0] == *(p+0)
+    printf("%lu\n", sizeof(p[0]));//1
+    printf("%lu\n", sizeof(&p));//8
+    printf("%lu\n", sizeof(&p+1));//4/8
+    printf("%lu\n", sizeof(&p[0]+1));//4/8
+    printf("*****************\n");
     
+    printf("%p\n", strlen(p));//6
+    printf("%lu\n", strlen(p + 1));//5
+//    printf("%lu\n", strlen(*p));//err
+//    printf("%lu\n", strlen(p[0]));//err
+    printf("%lu\n", strlen(&p));//随机
+    printf("%lu\n", strlen(&p+1));//随机
+    printf("%lu\n", strlen(&p[0]+1));//5
+    printf("*****************\n");
+    //二维数组大小
+    int er[3][4] = {0};
+    printf("%lu\n", sizeof(er));//48这里计算整个数组三行四列
+    printf("%lu\n", sizeof(er[0][0]));//4
+    //er[0]是第一行的数组名sizeof(er[0])把数组名单独放在sizeof内，计算数组的大小
+    printf("%lu\n", sizeof(er[0]));//16,
+    //er[0]是第一行数组名 把数组名放到表达式内则改数组名表示此时时首元素地址
+    printf("%lu\n", sizeof(er[0]+1));//4/8
+    printf("%lu\n", sizeof(*(er[0]+1)));//4
+    //er是二维数组名加1进行运算时它此时则表示首元素地址加1跳过第一行
+    printf("%lu\n", sizeof(er +1));//4/8第二行的地址数组的地址
+    printf("%lu\n", sizeof(*(er+1)));//16等价于sizeof(a[1])
+    printf("%lu\n", sizeof(&er[0]+1));//4/8第二行地址先取地址在加一
+    printf("%lu\n", sizeof(*(&er[0]+1)));//16第二行数组的大小
+    printf("%lu\n", sizeof(*er));//16第一行数组的大小
+    //sizeof内部的表达式是不参与真是的运算的，只是根据它的类型来计算它的大小
+    printf("%lu\n", sizeof(er[3]));//16他不会真的去访问，
+    printf("*****************\n");
+
     return 0;
 }
---
+++ 4.21/4.21/main.c
@@ -0,0 +1,102 @@
+//
+//  main.c
+//  4.21
+//
+//  Created by Rido on 2022/4/21.
+//
+
+#include <stdio.h>
+//指针笔试题
+//NO1:
+//int main(int argc, const char * argv[]) {
+//
+//    int a[5] = {1, 2, 3, 4, 5};
+//    int *ptr = (int*)(&a + 1);//跳过数组的地址还是数组的地址，数组的地址不能放到int*里面 所以要把它强转成int*
+//    printf("%d,%d\n", *(a+1), *(ptr -1));
+//    return 0;
+//}
+//NO2:
+//已知，结构体Test类型的变量大小是20字节
+//struct Test{
+//    int Num;
+//    char * pcName;
+//    short sDate;
+//    char cha[2];
+//    short sBa[4];
+//} *p;
+////假设p 的值为0x100000.如下表达式的值分别为多少
+////指针+- 整数取决于指针的类型
+//int main() {
+//
+//    p = (struct Test*)0x100000;
+//    printf("%d\n", sizeof(*p));
+//    //p是结构体类型大小是20字节，对它加一跳过20字节16进制表示14
+//    printf("%p\n", p + 0x1);//0x100014
+//    //强转无符号整型==（long）or（int）整形加1就是1
+//    printf("%p\n",(unsigned long)p+0x1);//0x100001
+//    //强转无符号整型指针。加一跳过一个整形
+//    printf("%p\n",(unsigned int*)p+0x1);//0x100004
+//    return 0;
+//No3
+//int main() {
+//    int a[4] = {1, 2, 3, 4};
+//    int *ptr1 = (int*)(&a +1);
+//    int *ptr2 = (int*)((int)a + 1);
+////    printf("%p\n", *ptr2);
+////    printf("%p\n", a[0]);
+//    printf("%x\n", ptr1[-1]);//0x4
+//    printf("%d\n", *ptr2);//0x02000000
+//    return 0;
+//}
+//No4
+//int main()
+//{
+//    int a[3][2] = {(0,1),(2,3),(4,5)};//{1,3,5,0,0,0}逗号表达式取后面的值
+//    int *p;
+//    p = a[0];
+//    printf("%d\n", p[0]);//0
+//    return 0;
+//}
+//No5
+//int main()
+//{
+//    int a[5][5];
+//    int (*p)[4];
+//    p = a;
+//    //p[4]是把指向a[4]的地址向后移动4个int(*)[4]类型的地址
+//    printf("%d\n", &p[4][2]-&a[4][2]);//-4
+//    //%p把-4的反码直接当成地址打印出来
+//    printf("%p\n", &p[4][2]-&a[4][2]);//0xfffffffc
+//    return 0;
+//}
+//No5
+//int main() {
+//    int arr[2][5] ={1,2,3,4,5,6,7,8,9,10};
+//    int *ptr1 = (int*)(&arr+1);
+//    int *ptr2 = (int*)(*(arr+1));
+//    printf("%d\n", *(ptr1-1));//10
+//    printf("%d\n", *(ptr2-1));//5
+//    return 0;
+//}
+//No6
+//int main() {
+//    char *a[] = {"work", "at", "alibaba"};
+//    char ** pa = a;
+//    pa++;
+//    printf("%s\n", *pa);//at
+//    return 0;
+//}
+//No7
+int main() {
+    char * c[] = {"ENTER","NEW","POINT","FIRST"};//{"E","N","P","F"}相当于这几个字符的地址
+    char **cp[] = {c + 3, c + 2, c + 1, c};//{*first,*point*new,*enter}
+    char ***cpp = cp;
+    //用%s打印会从当前地址向后找直到找\0停止
+    //++相当于+= 所以++后值会变
+    printf("%s\n", **++cpp);//point
+    printf("%s\n", *--*++cpp + 3);//ew
+    printf("%s\n", *cpp[-2] + 3);//s t
+    printf("%s\n", cpp[-1][-1] +1);//ew
+    return 0;
+}
+
